5e9013368c9f9b0cc524b6387b9007dd6f2320a0
Odette Gallo <odette.gallo@hotmail.com>
2025-09-25 12:18:46 -0300

    incluir bootstrap en main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as bootstrap from '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const app = createApp(App)
 
 app.use(router)
-
+app.provide('bootstrap', bootstrap)
 app.mount('#app')
