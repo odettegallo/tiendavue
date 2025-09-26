b5107c409bab05b4384baf38cfb1dd09846ca260
Odette Gallo <odette.gallo@hotmail.com>
2025-09-26 18:46:01 -0300

    footer fixed bottom carrito

--- src/components/CartDetail.vue
+++ src/components/CartDetail.vue
@@ -34,6 +34,26 @@
       </div>
     </div>
   </div>
+
+  <footer class="footer fixed-bottom mt-5">
+    <div class="container">
+      <h5>TechGadget Store</h5>
+      <p>
+        Tu tienda de tecnología de confianza con lo último en gadgets y
+        accesorios de calidad.
+      </p>
+      <p class="mb-2">Email: contacto@techgadgetstore.com | Tel: +56 9 1234 5678</p>
+    </div>
+    <div class="d-flex justify-content-center gap-3 fs-1 mb-2">
+      <a href="#"><i class="bi bi-facebook" style="font-size: 30px;"></i></a>
+      <a href="#"><i class="bi bi-instagram" style="font-size: 30px;"></i></a>
+      <a href="#"><i class="bi bi-twitter-x" style="font-size: 30px;"></i></a>
+      <a href="#"><i class="bi bi-youtube" style="font-size: 30px;"></i></a>
+    </div>
+    <div class="footer__bottom">
+      © 2025 TechGadget Store. Todos los derechos reservados.
+    </div>
+  </footer>
 </template>
 
 <script setup>
